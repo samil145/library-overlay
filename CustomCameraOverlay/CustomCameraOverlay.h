b2ce77c8c6bdb95013660113706f34ae1c40cfc9
samil145 <samil.bayramli.sb@gmail.com>
2025-01-04 04:21:00 +0400

    Initial Commit

---
+++ CustomCameraOverlay/CustomCameraOverlay.h
@@ -0,0 +1,18 @@
+//
+//  CustomCameraOverlay.h
+//  CustomCameraOverlay
+//
+//  Created by Shamil Bayramli on 04.01.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomCameraOverlay.
+FOUNDATION_EXPORT double CustomCameraOverlayVersionNumber;
+
+//! Project version string for CustomCameraOverlay.
+FOUNDATION_EXPORT const unsigned char CustomCameraOverlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomCameraOverlay/PublicHeader.h>
+
+
---
+++ CustomCameraOverlayTests/CustomCameraOverlayTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraOverlayTests.swift
+//  CustomCameraOverlayTests
+//
+//  Created by Shamil Bayramli on 04.01.25.
+//
+
+import Testing
+@testable import CustomCameraOverlay
+
+struct CustomCameraOverlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
